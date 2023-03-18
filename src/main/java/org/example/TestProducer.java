04e3ad2cba9dd8555a698ae0d2d0e862fac9bd75
Chethan Rao <chethanrao@Chethans-MacBook-Pro.local>
2023-03-18 16:41:25 -0700

    apache kafka with test containers

---
+++ src/main/java/org/example/TestProducer.java
@@ -0,0 +1,19 @@
+package org.example;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class TestProducer {
+
+    private Producer<Long, String> producer;
+    private String topic;
+
+    public TestProducer(Producer<Long, String> producer, String topic) {
+        this.producer = producer;
+        this.topic = topic;
+    }
+
+    public void send(String message) {
+        ProducerRecord<Long, String> producerRecord = new ProducerRecord<>(topic, message);
+        producer.send(producerRecord);
+    }
+}
